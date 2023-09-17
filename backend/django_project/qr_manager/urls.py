50956355501f5cc22b2129c3ba8cc05e77722fb8
zrqr <pedrohnunessilva@gmail.com>
2023-09-17 02:33:33 +0000

    creating new_code API

--- backend/django_project/qr_manager/urls.py
+++ backend/django_project/qr_manager/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("qrcodes/", views.qr_codes),
-    path("qrcodes/<int:id>", views.qr_code_item)
+    path("qrcodes/<int:id>", views.qr_code_item),
+    path("new_code/", views.create_qrcode)
 ]
--- backend/django_project/qr_manager/views.py
+++ backend/django_project/qr_manager/views.py
@@ -5,6 +5,8 @@ from .serializers import QrCodeSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import datetime
+import secrets
 
 # Create your views here.
 def home(request):
@@ -44,3 +46,21 @@ def qr_code_item(request, id):
     if request.method == "DELETE":
         qrcode.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["POST"]) 
+def create_qrcode(request):
+
+    if request.method == "POST":
+        new_code = {}
+
+        new_code["date_created"] = datetime.datetime.today().date()
+        new_code["name"] = request.data["name"]
+        new_code["date_finish"] = request.data["date_finish"]
+        new_code["token"] = secrets.token_urlsafe(32)
+
+
+        serializer = QrCodeSerializer(data = new_code)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
