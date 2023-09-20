75bd167ba34b838a90e2e6ba0f3dcbaef357b77d
zrqr <pedrohnunessilva@gmail.com>
2023-09-20 06:40:31 +0000

    uppdates

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
-FROM mcr.microsoft.com/devcontainers/python:1-3.11-bullseye
+FROM python:3.11-slim
+
+COPY . .
+
+RUN apt-get update
+RUN apt-get install ffmpeg -y
+RUN apt-get install python3-libgpiod -y
+RUN pip install -r requirements.txt
+
+
--- backend/django_project/qr_camscanner.py
+++ backend/django_project/qr_camscanner.py
@@ -1,6 +1,7 @@
 from os import path, environ
 from sys import path as sys_path
 from django import setup
+from gate_controller import GateRelay
 
 sys_path.append("setting.py")    
 environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
@@ -12,6 +13,8 @@ from qr_manager.views import check_qrcode
 from rest_framework import status
 
 
+gate = GateRelay()
+
 def loopscan():
     print("START")
     cap = cv2.VideoCapture("http://192.168.0.7:8081/video.mjpg")
@@ -19,25 +22,26 @@ def loopscan():
     detector = cv2.QRCodeDetector()
 
     while True:
-        _, img = cap.read()
+        ret, img = cap.read()
     
-        token, bbox, _ = detector.detectAndDecode(img)
+        if ret:
+            if count %% 30 == 0:
+                token, bbox, _ = detector.detectAndDecode(img)
 
-        # check if there is a QRCode in the image
-        if token:
-            ret = check_qrcode(token)
-            if ret == status.HTTP_200_OK:
-                open_the_gate()
+                # check if there is a QRCode in the image
+                if token:
+                    ret = check_qrcode(token)
+                    if ret == status.HTTP_200_OK:
+                        gate.open()
+                        print("It is open")
 
-            if ret == status.HTTP_401_UNAUTHORIZED:
-                print("Unauthorized")
+                    if ret == status.HTTP_401_UNAUTHORIZED:
+                        print("Unauthorized")
 
-        #cv2.imshow("QRCODEscanner", img)    
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-def open_the_gate():
-    print("It is open")
+                #cv2.imshow("QRCODEscanner", img)    
+                if cv2.waitKey(1) == ord("q"):
+                    break
+            count=count+1
 
 if __name__ == "__main__":
     loopscan()
