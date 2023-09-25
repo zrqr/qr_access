e13a1cd948fca79c8356140ad2a81be7473da504
zrqr <pedrohnunessilva@gmail.com>
2023-09-25 01:00:52 -0700

    frontend updates

--- frontend/react/src/components/QrCard.js
+++ frontend/react/src/components/QrCard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Resizer from "react-image-file-resizer";
 
 function QrCard({ task }) {
   const [finishDate, setFinishDate] = useState(task.date_finish);
@@ -16,14 +17,38 @@ function QrCard({ task }) {
     // and then remove the task from the UI when the request is successful
   };
 
+  const resizeFile = async (file) => {
+    try {
+      const uri = await new Promise((resolve) => {
+        Resizer.imageFileResizer(
+          file,
+          200, // Width
+          200, // Height
+          "PNG",
+          100,  // Quality
+          0,
+          (uri) => {
+            console.log(uri);
+            resolve(uri);
+          },
+          "base64",
+          200,
+          200
+        );
+      });
+      setImageSrc(uri); // Update the state with the resized image URI
+    } catch (error) {
+      console.error('Error resizing image:', error);
+    }
+  };
+
   useEffect(() => {
     // Fetch the image from the API when the component mounts
     fetch(`http://127.0.0.1:8000/image/${task.id}`)
       .then((response) => response.blob())
       .then((imageBlob) => {
-        // Create a data URL for the image
-        const imageUrl = URL.createObjectURL(imageBlob);
-        setImageSrc(imageUrl);
+        // Resize the image and get the resized URI
+        resizeFile(imageBlob);
       })
       .catch((error) => {
         console.error('Error fetching image:', error);
