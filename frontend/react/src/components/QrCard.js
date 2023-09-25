bd50b657ee9b4e8245415ceb5ff8b15f03309c8c
zrqr <pedrohnunessilva@gmail.com>
2023-09-25 00:37:42 -0700

    adding qr code

--- frontend/react/src/components/QrCard.js
+++ frontend/react/src/components/QrCard.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function QrCard({ task }) {
-  const [finishDate, setFinishDate] = useState(task.finishDate);
+  const [finishDate, setFinishDate] = useState(task.date_finish);
+  const [imageSrc, setImageSrc] = useState(null);
 
   const handleUpdateFinishDate = () => {
     // You can implement your update logic here
@@ -15,14 +16,35 @@ function QrCard({ task }) {
     // and then remove the task from the UI when the request is successful
   };
 
+  useEffect(() => {
+    // Fetch the image from the API when the component mounts
+    fetch(`http://127.0.0.1:8000/image/${task.id}`)
+      .then((response) => response.blob())
+      .then((imageBlob) => {
+        // Create a data URL for the image
+        const imageUrl = URL.createObjectURL(imageBlob);
+        setImageSrc(imageUrl);
+      })
+      .catch((error) => {
+        console.error('Error fetching image:', error);
+      });
+
+    // Clean up the data URL when the component unmounts
+    return () => {
+      if (imageSrc) {
+        URL.revokeObjectURL(imageSrc);
+      }
+    };
+  }, [task.id]);
+
   return (
     <div className="qr-card">
       <h2>{task.name}</h2>
       <div>
-        <div>Date Created: {task.dateCreated}</div>
+        <div>Date Created: {task.date_created}</div>
         <div>Finish Date: {finishDate}</div>
       </div>
-      <img src={task.image} alt={task.name} />
+      {imageSrc && <img src={imageSrc} alt={task.name} />}
       <div>
         <button onClick={handleUpdateFinishDate}>Update Finish Date</button>
         <button onClick={handleDeleteTask}>Delete</button>
