6d87168290ae64995182c7f3769fba420e61c0bc
zrqr <pedrohnunessilva@gmail.com>
2023-10-05 05:25:43 +0000

    updates

--- arduino/arduino_server.ino
+++ arduino/arduino_server.ino
@@ -24,12 +24,18 @@ const int KEYPAD_D0_PIN = 2;
 const int KEYPAD_D1_PIN = 3;
 const int REDLED_PIN = 4;
 const int BUZZ_PIN = 5;
+volatile bool check = false;
 
 // Create an instance of the server
 WebServer server(80);
+WiFiClient client;
+IPAddress ipn(192,168,0,17); 
 Wiegand wiegand;
 
 StaticJsonDocument<250> jsonDocument;
+volatile char currpass[10];
+
+
 char buffer[250];
 
 void setup()
@@ -52,7 +58,7 @@ void setup()
   pinMode(SOCIAL_PIN, OUTPUT);
   server.on(UriBraces("/open_the_gate/{}"), gateHandler);     
   server.begin(); 
-
+  currpass[0] = '\0';
 
   //Install listeners and initialize Wiegand reader
   wiegand.onReceive(receivedData, "Card readed: ");
@@ -68,6 +74,14 @@ void setup()
   pinStateChanged();
 }
 
+void loop() {
+
+  noInterrupts();
+  wiegand.flush();
+  interrupts();
+  callAPI();
+  server.handleClient(); 
+}
 
 // When any of the pins have changed, update the state of the wiegand library
 void pinStateChanged() {
@@ -75,14 +89,93 @@ void pinStateChanged() {
   wiegand.setPin1State(digitalRead(KEYPAD_D1_PIN));
 }
 
-void receivedData(uint8_t* data, uint8_t bits, const char* message) {
+void callAPI(){
+    bool check_state;
+    check_state = check;
+    if (check_state==true) {
+      const uint16_t port = 8000;
+      const char * host = "192.168.0.17"; // ip or dns
 
+      // WRITE --------------------------------------------------------------------------------------------
+      
+      if (!client.connect(host, port)) {
+          Serial.println("Connection failed.");
+          Serial.println("Waiting 5 seconds before retrying...");
+          return;
+      }
+      noInterrupts();
+      char frozenChar[10];  // Make sure bufferSize is large enough to hold the data
+      for (int i = 0; i < 10; i++) {
+        frozenChar[i] = currpass[i];
+      }
+      char footer[100] = " HTTP/1.1\r\nHost: 192.168.0.17\r\n" ;
+      char request[1000]; // Make sure the buffer is large enough
+      strcpy(request, "GET /check_password/");
+      strcat(request, frozenChar);
+      strcat(request, footer);
+      
+      for (int i = 0; i < 10; i++) {
+        currpass[i] = '\0';
+      }
+      check=false;
+      interrupts();
+      client.print(request);
+      readResponse();
+    }
+  
+}
+
+void receivedData(uint8_t* data, uint8_t bits, const char* message) {
     //Print value in HEX
+    noInterrupts();
+    char nonVolatileBuffer[10];
+
     uint8_t bytes = (bits+7)/8;
     for (int i=0; i<bytes; i++) {
-        Serial.print(data[i] & 0xF, 16);
+        for (int i = 0; i < 10; i++) {
+          nonVolatileBuffer[i] = currpass[i];
+        }
+        uint8_t result = data[i] & 0xF;
+        char hexChar;
+
+        if (result < 10) {
+          hexChar = '0' + result; // Convert to ASCII character '0' to '9'
+        } else {
+          hexChar = 'A' + (result - 10); // Convert to ASCII character 'A' to 'F'
+        }
+        currpass[strlen(nonVolatileBuffer)] = hexChar;
+        for (int i = 0; i < 10; i++) {
+          nonVolatileBuffer[i] = currpass[i];
+        }
+        Serial.println(nonVolatileBuffer);
+
+        if (strlen(nonVolatileBuffer)>=6) {
+            
+            check = true;
+            
+        }
+
     }
+    interrupts();
+}
+
+void readResponse(){
+  
+   if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
     Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+
+    // do nothing forevermore:
+    for(;;)
+      ;
+  }
 }
 
 void gateHandler(){
@@ -106,12 +199,6 @@ void gateHandler(){
   delay(3000);
 }
 
-void loop() {
-  server.handleClient(); 
-  noInterrupts();
-  wiegand.flush();
-  interrupts();
-}
 void ledAPI(){  
   
   String arg = server.pathArg(0);
